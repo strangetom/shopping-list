3a187721fc421673459668399377c1da14a43500
Tom Strange <tpstrange@gmail.com>
2022-12-29 17:02:01 +0000

    Feature: Replace 'D' button with a check mark

--- static/js/index.js
+++ static/js/index.js
@@ -119,7 +119,7 @@ class ListItem extends HTMLLIElement {
         let td_right = document.createElement("td");
         tr.appendChild(td_right);
         let delete_button = document.createElement("button");
-        delete_button.innerText = "D";
+        delete_button.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="${color}" style="scale: 1.3"><path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z"/></svg>`;
         delete_button.addEventListener("click", this.done.bind(this));
         td_right.appendChild(delete_button);
         this.updateData();
